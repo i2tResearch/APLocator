47ac3a1956c22e153f0fbb56c81ca0b422cea83f
lvbernal <lvbernal@gmail.com>
2020-07-07 19:52:03 -0500

    json encoding - degree symbol

--- src/storage/storage.py
+++ src/storage/storage.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import codecs
 import time
 import json
 
 
 def save(file_path, content):
-    content_str = json.JSONEncoder().encode(content)
+    content_str = json.JSONEncoder(ensure_ascii=False).encode(content)
 
     suffix = time.strftime("%Y%m%d_%H%M%S")
     file_name = file_path + "_" + suffix + ".json"
 
-    with open(file_name, "w+") as f:
+    with codecs.open(file_name, "w+", 'utf-8') as f:
         f.write(content_str)
 
     print("file saved", file_name)
